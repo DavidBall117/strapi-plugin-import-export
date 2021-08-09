d1c3549874e81a3154e24825e49bd650d66f496a
David Ball <davidball117@gmail.com>
2021-08-09 01:19:58 -0400

    Formatted code using .editorcongfig for consistency.

--- admin/src/containers/HomePage/useHomePage.js
+++ admin/src/containers/HomePage/useHomePage.js
@@ -2,176 +2,176 @@ import { useState } from "react";
 import { request } from "strapi-helper-plugin";
 
 const excludedSources = [
-    'admin',
-    'users-permissions',
-    'upload',
-    'i18n'
+  'admin',
+  'users-permissions',
+  'upload',
+  'i18n'
 ];
 
 const strapiProperties = [
-    'id',
-    'published_at',
-    'created_at',
-    'updated_at',
-    'created_by',
-    'updated_by'
+  'id',
+  'published_at',
+  'created_at',
+  'updated_at',
+  'created_by',
+  'updated_by'
 ];
 
 const mediaProperties = [
-    'name',
-    'alternativeText',
-    'caption',
-    'width',
-    'height',
-    'formats',
-    'hash',
-    'ext',
-    'mime',
-    'size',
-    'url',
-    'previewUrl',
-    'provider',
-    'provider_metadata'
+  'name',
+  'alternativeText',
+  'caption',
+  'width',
+  'height',
+  'formats',
+  'hash',
+  'ext',
+  'mime',
+  'size',
+  'url',
+  'previewUrl',
+  'provider',
+  'provider_metadata'
 ];
 
 export default () => {
-    const getDefaultFileName = (sourceName) => {
-        const name = 'import-export';
-        const ext = '.json';
-        const now = new Date();
-        const date = `${now.getFullYear()}-${String(now.getMonth() + 1).padStart(2, '0')}-${String(now.getDate()).padStart(2, '0')}`;
-        return sourceName ? `${name}-${sourceName.toLocaleLowerCase()}${ext}_${date}` : `${name}${ext}_${date}`;
-    };
+  const getDefaultFileName = (sourceName) => {
+    const name = 'import-export';
+    const ext = '.json';
+    const now = new Date();
+    const date = `${now.getFullYear()}-${String(now.getMonth() + 1).padStart(2, '0')}-${String(now.getDate()).padStart(2, '0')}`;
+    return sourceName ? `${name}-${sourceName.toLocaleLowerCase()}${ext}_${date}` : `${name}${ext}_${date}`;
+  };
 
-    const [loading, setLoading] = useState(false);
-    const [removeStrapiProperties, setRemoveStrapiProperties] = useState(true);
-    const [removeMedia, setRemoveMedia] = useState(false);
-    const [sources, setSources] = useState([{ label: 'all', value: 'all' }]);
-    const [source, setSource] = useState('all');
-    const [fileName, setFileName] = useState(getDefaultFileName());
-    const [data, setData] = useState([]);
-    const [dataString, setDataString] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [removeStrapiProperties, setRemoveStrapiProperties] = useState(true);
+  const [removeMedia, setRemoveMedia] = useState(false);
+  const [sources, setSources] = useState([{ label: 'all', value: 'all' }]);
+  const [source, setSource] = useState('all');
+  const [fileName, setFileName] = useState(getDefaultFileName());
+  const [data, setData] = useState([]);
+  const [dataString, setDataString] = useState('');
 
-    const cleanStrapiProperties = (obj) => {
-        if (Array.isArray(obj)) {
-            obj.forEach((item) => {
-                cleanStrapiProperties(item);
-            });
-        } else if (obj && typeof obj === 'object') {
-            Object.getOwnPropertyNames(obj).forEach((key) => {
-                if (strapiProperties.indexOf(key) !== -1) {
-                    delete obj[key];
-                } else {
-                    cleanStrapiProperties(obj[key]);
-                }
-            });
+  const cleanStrapiProperties = (obj) => {
+    if (Array.isArray(obj)) {
+      obj.forEach((item) => {
+        cleanStrapiProperties(item);
+      });
+    } else if (obj && typeof obj === 'object') {
+      Object.getOwnPropertyNames(obj).forEach((key) => {
+        if (strapiProperties.indexOf(key) !== -1) {
+          delete obj[key];
+        } else {
+          cleanStrapiProperties(obj[key]);
         }
-    };
+      });
+    }
+  };
 
-    const cleanMedia = (obj) => {
-        // TODO
-    };
+  const cleanMedia = (obj) => {
+    // TODO
+  };
 
-    const onSetRemoveStrapiPropertiesChange = (bool) => {
-        setRemoveStrapiProperties(bool);
-        if (data.length > 0) {
-            const dataCopy = JSON.parse(JSON.stringify(data));
-            if (bool) {
-                cleanStrapiProperties(dataCopy);
-            }
-            if (removeMedia) {
-                cleanMedia(dataCopy);
-            }
-            setDataString(JSON.stringify(dataCopy, null, 2));
+  const onSetRemoveStrapiPropertiesChange = (bool) => {
+    setRemoveStrapiProperties(bool);
+    if (data.length > 0) {
+      const dataCopy = JSON.parse(JSON.stringify(data));
+      if (bool) {
+        cleanStrapiProperties(dataCopy);
+      }
+      if (removeMedia) {
+        cleanMedia(dataCopy);
+      }
+      setDataString(JSON.stringify(dataCopy, null, 2));
+    }
+  };
+
+  const onSetRemoveMedia = (bool) => {
+    setRemoveMedia(bool);
+    if (data.length > 0) {
+      const dataCopy = JSON.parse(JSON.stringify(data));
+      if (bool) {
+        cleanMedia(dataCopy);
+      }
+      if (removeStrapiProperties) {
+        cleanStrapiProperties(dataCopy);
+      }
+      setDataString(JSON.stringify(dataCopy, null, 2));
+    }
+  };
+
+  const isValidSource = (fetchedSource) => {
+    return !excludedSources.includes(fetchedSource);
+  };
+
+  const fetchSources = async () => {
+    try {
+      const response = await request('/content-type-builder/content-types', { method: 'GET' });
+      const fetchedSources = [];
+      for (let i = 0; i < response.data.length; i++) {
+        if (isValidSource(response.data[i].plugin)) {
+          fetchedSources.push({
+            label: response.data[i].apiID,
+            value: response.data[i].apiID,
+          });
         }
-    };
+      }
+      setSources(prevState => prevState.concat(fetchedSources));
+    } catch (err) {
+      console.error(err);
+      strapi.notification.toggle({
+        type: 'warning',
+        message: 'An error occured while fetching data sources.'
+      });
+    }
+  };
 
-    const onSetRemoveMedia = (bool) => {
-        setRemoveMedia(bool);
-        if (data.length > 0) {
-            const dataCopy = JSON.parse(JSON.stringify(data));
-            if (bool) {
-                cleanMedia(dataCopy);
-            }
-            if (removeStrapiProperties) {
-                cleanStrapiProperties(dataCopy);
-            }
-            setDataString(JSON.stringify(dataCopy, null, 2));
-        }
-    };
+  const fetchSourceData = async () => {
+    setLoading(true);
+    try {
+      const response = await request(`/import-export/raw-data?source=${source}`, { method: 'GET' });
+      setData(JSON.parse(JSON.stringify(response)));
+      if (removeStrapiProperties) {
+        cleanStrapiProperties(response);
+      }
+      if (removeMedia) {
+        cleanMedia(response);
+      }
+      setDataString(JSON.stringify(response, null, 2));
+    } catch (err) {
+      console.error(err);
+      strapi.notification.toggle({
+        type: 'warning',
+        message: 'An error occured while fetching source data.'
+      });
+    }
+    setLoading(false);
+  };
 
-    const isValidSource = (fetchedSource) => {
-        return !excludedSources.includes(fetchedSource);
-    };
+  const downloadSourceData = () => {
+    const element = document.createElement('a');
+    const file = new Blob([dataString], { type: 'text/plain;charset=utf-8' });
+    element.href = URL.createObjectURL(file);
+    element.download = fileName;
+    document.body.appendChild(element); // Required for Firefox
+    element.click();
+  };
 
-    const fetchSources = async () => {
-        try {
-            const response = await request('/content-type-builder/content-types', { method: 'GET' });
-            const fetchedSources = [];
-            for (let i = 0; i < response.data.length; i++) {
-                if (isValidSource(response.data[i].plugin)) {
-                    fetchedSources.push({
-                        label: response.data[i].apiID,
-                        value: response.data[i].apiID,
-                    });
-                }
-            }
-            setSources(prevState => prevState.concat(fetchedSources));
-        } catch (err) {
-            console.error(err);
-            strapi.notification.toggle({
-                type: 'warning',
-                message: 'An error occured while fetching data sources.'
-            });
-        }
-    };
-
-    const fetchSourceData = async () => {
-        setLoading(true);
-        try {
-            const response = await request(`/import-export/raw-data?source=${source}`, { method: 'GET' });
-            setData(JSON.parse(JSON.stringify(response)));
-            if (removeStrapiProperties) {
-                cleanStrapiProperties(response);
-            }
-            if (removeMedia) {
-                cleanMedia(response);
-            }
-            setDataString(JSON.stringify(response, null, 2));
-        } catch (err) {
-            console.error(err);
-            strapi.notification.toggle({
-                type: 'warning',
-                message: 'An error occured while fetching source data.'
-            });
-        }
-        setLoading(false);
-    };
-
-    const downloadSourceData = () => {
-        const element = document.createElement('a');
-        const file = new Blob([dataString], { type: 'text/plain;charset=utf-8' });
-        element.href = URL.createObjectURL(file);
-        element.download = fileName;
-        document.body.appendChild(element); // Required for Firefox
-        element.click();
-    };
-
-    return {
-        getDefaultFileName,
-        loading,
-        removeStrapiProperties,
-        removeMedia,
-        sources,
-        source,
-        setSource,
-        fileName,
-        setFileName,
-        dataString,
-        onSetRemoveStrapiPropertiesChange,
-        onSetRemoveMedia,
-        fetchSources,
-        fetchSourceData,
-        downloadSourceData
-    };
+  return {
+    getDefaultFileName,
+    loading,
+    removeStrapiProperties,
+    removeMedia,
+    sources,
+    source,
+    setSource,
+    fileName,
+    setFileName,
+    dataString,
+    onSetRemoveStrapiPropertiesChange,
+    onSetRemoveMedia,
+    fetchSources,
+    fetchSourceData,
+    downloadSourceData
+  };
 };
--- admin/src/containers/ImportPage/useImportPage.js
+++ admin/src/containers/ImportPage/useImportPage.js
@@ -2,99 +2,99 @@ import { useState } from "react";
 import { request } from "strapi-helper-plugin";
 
 export default () => {
-    const [loading, setLoading] = useState(false);
-    const [file, setFile] = useState(null);
-    const [isDragging, setIsDragging] = useState(false);
-    const [data, setData] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [file, setFile] = useState(null);
+  const [isDragging, setIsDragging] = useState(false);
+  const [data, setData] = useState('');
 
-    const handleDragEnter = () => {
-        setIsDragging(true);
-    };
+  const handleDragEnter = () => {
+    setIsDragging(true);
+  };
 
-    const handleDragLeave = () => {
-        setIsDragging(false);
-    };
+  const handleDragLeave = () => {
+    setIsDragging(false);
+  };
 
-    const handleDrop = (event) => {
-        event.preventDefault();
-        setIsDragging(false);
-        setFile(event.dataTransfer.files[0]);
-    };
+  const handleDrop = (event) => {
+    event.preventDefault();
+    setIsDragging(false);
+    setFile(event.dataTransfer.files[0]);
+  };
 
-    const handleDragOver = (event) => {
-        event.preventDefault();
-        event.stopPropagation();
-    };
+  const handleDragOver = (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+  };
 
-    const onFileChange = (event) => {
-        setFile(event.target.files[0]);
-    };
+  const onFileChange = (event) => {
+    setFile(event.target.files[0]);
+  };
 
-    const readFileContent = (file) => {
-        const reader = new FileReader();
-        return new Promise((resolve, reject) => {
-            reader.onload = (event) => resolve(event.target.result);
-            reader.onerror = reject;
-            reader.readAsText(file);
+  const readFileContent = (file) => {
+    const reader = new FileReader();
+    return new Promise((resolve, reject) => {
+      reader.onload = (event) => resolve(event.target.result);
+      reader.onerror = reject;
+      reader.readAsText(file);
+    });
+  };
+
+  const loadFile = async () => {
+    try {
+      if (file) {
+        const fileContent = await readFileContent(file);
+        setData(fileContent);
+      }
+    } catch (err) {
+      console.error(err);
+      strapi.notification.toggle({
+        type: 'warning',
+        message: 'An error occured while trying to read the input file.'
+      });
+    }
+  };
+
+  const uploadFile = async () => {
+    setLoading(true);
+    try {
+      const parsedData = JSON.parse(data);
+      const response = await request('/import-export/raw-data', {
+        method: 'POST',
+        body: parsedData
+      });
+      if (response.success) {
+        strapi.notification.toggle({
+          type: 'success',
+          message: 'Upload successful.'
         });
-    };
-
-    const loadFile = async () => {
-        try {
-            if (file) {
-                const fileContent = await readFileContent(file);
-                setData(fileContent);
-            }
-        } catch (err) {
-            console.error(err);
-            strapi.notification.toggle({
-                type: 'warning',
-                message: 'An error occured while trying to read the input file.'
-            });
-        }
-    };
-
-    const uploadFile = async () => {
-        setLoading(true);
-        try {
-            const parsedData = JSON.parse(data);
-            const response = await request('/import-export/raw-data', {
-                method: 'POST',
-                body: parsedData
-            });
-            if (response.success) {
-                strapi.notification.toggle({
-                    type: 'success',
-                    message: 'Upload successful.'
-                });
-            } else {
-                strapi.notification.toggle({
-                    type: 'warning',
-                    message: `An error occured while saving data file to database.
+      } else {
+        strapi.notification.toggle({
+          type: 'warning',
+          message: `An error occured while saving data file to database.
                         ${response.message ? `Error Message: ${response.message}` : ''}`
-                });
-            }
-        } catch (err) {
-            console.error(err);
-            strapi.notification.toggle({
-                type: 'warning',
-                message: 'An error occured while submitting data to endpoint.'
-            });
-        }
-        setLoading(false);
-    };
+        });
+      }
+    } catch (err) {
+      console.error(err);
+      strapi.notification.toggle({
+        type: 'warning',
+        message: 'An error occured while submitting data to endpoint.'
+      });
+    }
+    setLoading(false);
+  };
 
-    return {
-        loading,
-        file,
-        isDragging,
-        data,
-        handleDragEnter,
-        handleDragLeave,
-        handleDrop,
-        handleDragOver,
-        onFileChange,
-        loadFile,
-        uploadFile
-    };
+  return {
+    loading,
+    file,
+    isDragging,
+    data,
+    handleDragEnter,
+    handleDragLeave,
+    handleDrop,
+    handleDragOver,
+    onFileChange,
+    loadFile,
+    uploadFile
+  };
 };
