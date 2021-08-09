267d49d5e62025fcb46fbe21db70d68c9cd503dd
David Ball <davidball117@gmail.com>
2021-08-09 00:28:11 -0400

    Added back in default route.

--- controllers/import-export.js
+++ controllers/import-export.js
@@ -1,6 +1,12 @@
 'use strict';
 
 module.exports = {
+  index: async (ctx) => {
+    ctx.send({
+      message: 'ok'
+    });
+  },
+
   getRawData: async (ctx) => {
     const responseData = [];
     // If source is all, fetch all data, else fetch specific source data.
